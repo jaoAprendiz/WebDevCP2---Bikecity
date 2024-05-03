a92776b4c4ab9a6c7f9a052cef9d1f7f3f122a5f
Felipe <felipenafe@gmail.com>
2024-05-03 20:01:29 -0300

    Felipe Fernandes rm:554598

---
+++ scriptlogin.js
@@ -0,0 +1,10 @@
+function validar(){
+    //declarando a variavel
+    let usuario =document.getElementById("usuario").value;
+    let senha =document.getElementById("senha").value;
+    if(usuario == "Admin" && senha === "12345"){
+        window.open("quiz.html")
+    }else{
+        alert("usuario e senha inválidos")
+    }
+}
